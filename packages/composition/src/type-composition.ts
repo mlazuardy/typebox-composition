1a37c2ae09bb0593b657f636532601d8aafd12a3
mlazuardy <lazuardymichael@gmail.com>
2023-08-27 20:54:59 +0700

    feat: ability to customize per-schema messages

--- packages/composition/src/type-composition.ts
+++ packages/composition/src/type-composition.ts
@@ -7,6 +7,8 @@ import {
 } from "./interfaces";
 import { Value, ValueError } from "@sinclair/typebox/value";
 import { formatMessage, getErrorInfo } from "./errors";
+import { isObjectEmpty } from "./utils";
+import { LocalMessage } from "./interfaces/message.interface";
 
 export class TypeComposition {
   private lang = "en";
@@ -38,13 +40,36 @@ export class TypeComposition {
     };
   }
 
+  private getCurrentMessages(lang?: string) {
+    return this.messages[this.getLang(lang)];
+  }
+
+  private getMessageValue(key: string, lang?: string) {
+    return this.getCurrentMessages(lang)?.[key];
+  }
+
+  private getCustomMessageKey(
+    messageKey: string,
+    messages: Record<string, LocalMessage>,
+    lang?: string,
+  ) {
+    const fixedLang = this.getLang(lang);
+    const messageVal = messages?.[fixedLang]?.[messageKey];
+
+    return messageVal || this.getMessageValue(messageKey, lang);
+  }
+
   getErrorMessage(error: ValueError, lang?: string) {
     const kind = error.schema[Symbol.for("TypeBox.Kind") as any];
     const messages = this.messages[this.getLang(lang)];
     const field = error.path.replace("/", "").split("/").join(".");
     const fieldOrTitle = error.schema.title || field;
     const { messageKey, expected, ...rest } = getErrorInfo(error);
-    const messageVal = messages[messageKey];
+    let messageVal = messages[messageKey];
+
+    if (!isObjectEmpty(error.schema.messages)) {
+      messageVal = this.getCustomMessageKey(messageKey, error.schema.messages);
+    }
 
     const payload: SchemaError = {
       field,
